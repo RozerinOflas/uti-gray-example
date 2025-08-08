238d0cce528c6a4c2c69e7368d62c990e858694f
Rozerin Oflas <oflasrozerin@gmail.com>
2025-08-08 17:10:42 +0300

    edited

--- src/executors/Scalling.py
+++ src/executors/Scalling.py
@@ -31,13 +31,14 @@ class Scalling(Component):
 
     def run(self):
         img = Image.get_frame(img=self.image, redis_db=self.redis_db)
-        imgA = Image.get_frame(img=self.image2, redis_db=self.redis_db)
         img.value = self.scaling(img.value)
-        imgA.value = self.scaling(imgA.value)
         self.image = Image.set_frame(img=img, package_uID=self.uID, redis_db=self.redis_db)
+
+        imgA = Image.get_frame(img=self.imageA, redis_db=self.redis_db)
+        imgA.value = self.scaling(imgA.value)
         self.imageA = Image.set_frame(img=imgA, package_uID=self.uID, redis_db=self.redis_db)
 
-        packageModel = build_responseScale(context=self, output_images=[])
+        packageModel = build_responseScale(context=self, output_images=[self.image, self.imageA])
         return packageModel
 
 
