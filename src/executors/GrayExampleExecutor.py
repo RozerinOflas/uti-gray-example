9d7a3c98573aa09052675a1c3a418f62318a7e57
Rozerin Oflas <oflasrozerin@gmail.com>
2025-08-08 09:17:34 +0300

    edited

--- src/executors/GrayExampleExecutor.py
+++ src/executors/GrayExampleExecutor.py
@@ -1,6 +1,7 @@
 """
     It is one of the preprocessing components in which the image is rotated.
 """
+
 import os
 import cv2
 import sys
@@ -22,17 +23,34 @@ class GrayExampleExecutor(Component):
         self.keep_side = self.request.get_param("KeepSide")
         self.image = self.request.get_param("inputImage")
 
-
     @staticmethod
     def bootstrap(config: dict) -> dict:
         return {}
 
-    def gray(self,img):
-        """"
-          Converts the image to grayscale.
-        """
-        return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    def rotation(self, image):
+        if self.keep_side == True:
+            height, width = image.shape[:2]
+            image_center = (width / 2, height / 2)
+            rotation_arr = cv2.getRotationMatrix2D(image_center, self.rotation_degree, 1)
+            abs_cos = abs(rotation_arr[0, 0])
+            abs_sin = abs(rotation_arr[0, 1])
+            bound_w = int(height * abs_sin + width * abs_cos)
+            bound_h = int(height * abs_cos + width * abs_sin)
+            rotation_arr[0, 2] += bound_w / 2 - image_center[0]
+            rotation_arr[1, 2] += bound_h / 2 - image_center[1]
+            img_rotation = cv2.warpAffine(image, rotation_arr, (bound_w, bound_h))
 
+            return img_rotation
+
+        elif self.keep_side == False:
+            height, width = image.shape[:2]
+            rotation_arr = cv2.getRotationMatrix2D((height / 2, width / 2), self.rotation_degree, 1)
+            img_rotation = cv2.warpAffine(image, rotation_arr, (height, width))
+
+            return img_rotation
+
+    def gray(self,img):
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     def run(self):
         img = Image.get_frame(img=self.image, redis_db=self.redis_db)
--- src/utils/response.py
+++ src/utils/response.py
@@ -1,14 +1,14 @@
 
 from sdks.novavision.src.helper.package import PackageHelper
-from components.GrayExample.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, GrayExampleExecutorOutputs, GrayExampleExecutorResponse, GrayExampleExecutor, OutputImage
+from components.GrayExample.src.models.PackageModel import PackageModel, PackageConfigs,ConfigExecutor, GrayExampleExecutor, GrayExampleExecutorOutputs, GrayExampleExecutorResponse, OutputImage
 
 
 def build_response(context):
     outputImage = OutputImage(value=context.image)
-    GrayExampleExecutorOutputs = GrayExampleExecutorOutputs(outputImage=outputImage)
-    GrayExampleExecutorResponse = GrayExampleExecutorResponse(outputs=GrayExampleExecutorOutputs)
-    GrayExampleExecutorExecutor = GrayExampleExecutorExecutor(value=GrayExampleExecutorResponse)
-    executor = ConfigExecutor(value=GrayExampleExecutorExecutor)
+    grayExampleExecutorOutputs = GrayExampleExecutorOutputs(outputImage=outputImage)
+    grayExampleExecutorResponse = GrayExampleExecutorResponse(outputs=grayExampleExecutorOutputs)
+    grayExampleExecutor = GrayExampleExecutor(value=grayExampleExecutorResponse)
+    executor = ConfigExecutor(value=grayExampleExecutor)
     packageConfigs = PackageConfigs(executor=executor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
     packageModel = package.build_model(context)
